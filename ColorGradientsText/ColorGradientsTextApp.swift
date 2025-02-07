f5ccd50160227cf42e2581c5b2a3bc4c77fd330f
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2025-02-07 17:09:09 +0500

    Initial Commit

---
+++ ColorGradientsText/ColorGradientsTextApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorGradientsTextApp.swift
+//  ColorGradientsText
+//
+//  Created by Elexoft on 07/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct ColorGradientsTextApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
